add145b581cbb5993d86cd3530c31213133b9f5b
KostasPapoulias <129172907+KostasPapoulias@users.noreply.github.com>
2024-04-11 18:06:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function loadGameScript() {
         let scriptElement = document.createElement('script');
-        scriptElement.src = "../Animals/script.js";
+        scriptElement.src = "../Animals/index.js";
         document.body.appendChild(scriptElement);
     }
 });
