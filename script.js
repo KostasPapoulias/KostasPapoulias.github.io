ac010627684dc392f1a4d73e0bd31935187ecbc3
kostas <kostaspapoulias@gmail.com>
2024-04-11 18:41:42 +0200

    updates

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     function overrideMenu() {
-        document.querySelector('.menu').innerHTML = `
+        document.querySelector('.index').innerHTML = `
             <div class="contain">
                 <div class="main_section" id="main_section"></div>
                 <div class="select_section" id="select_section"></div>
